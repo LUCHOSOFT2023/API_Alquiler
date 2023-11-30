de4f28854bfaa1b85b496a81d1ad534376176d0b
LUCHOSOFT2023 <magonzalez677@misena.edu.co>
2023-11-30 02:25:43 -0500

    primer commit

---
+++ models/alquiler.js
@@ -0,0 +1,34 @@
+const {Schema, model} = require('mongoose')
+
+const AlquilerSchema = Schema({
+
+    numero: {
+        type: Number,
+        unique: true,
+        required: [true, 'el número es obligatorio']
+    },
+
+    nombre_finca: {
+        type: String,
+        required: [true, 'El nombre de la finca es obligatorio']
+    },
+
+    direccion: {
+        type: String,
+        required: [true, 'La dirección de la finca es obligatorio']
+    },
+
+    valor_alquiler: {
+        type: Number,
+        required: [true, 'El precio del alquiler es obligatorio']
+    },
+
+    cantidad_dias:{
+        type: String,
+        required:[true, 'La cantidad de días es requerido']
+    }
+
+})
+
+//Exportar la función UsuarioSchema
+module.exports = model('Alquiler',AlquilerSchema)
